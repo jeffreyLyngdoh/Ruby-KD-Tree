88542657df08648158c745e98d24986a553ba135
jeffreyLyngdoh <Jeffrey.k.lyngdoh@gmail.com>
2023-08-02 14:55:48 -0400

    Adding some comments

--- driver.rb
+++ driver.rb
@@ -6,7 +6,6 @@ require 'securerandom'
 def driver()
     prompt = "0 - Quit, 1 - Create Tree, 2 - Insert Single, 3 - Insert Mass random, 4 - Delete Entry, 5 - K-Nearest Neighbor, 6 - Print JSON"
 
-
     puts prompt
     option = gets.chomp.to_i
 
@@ -20,8 +19,20 @@ def driver()
             puts "Please select a leaf size"
             leafsize = gets.chomp.to_i
 
+            while leafsize <= 0
+                puts "Please Select a valid leaf size greater than 0 less than 100"
+                leafsize = gets.chomp.to_i
+
+            end
+
             puts "Please select how many dimentions you want"
             dimensions = gets.chomp.to_i
+
+            while dimensions <= 0
+                puts "Please Select a valid number of dimensions greater than 0 less than 100"
+                leafsize = gets.chomp.to_i
+
+            end
 
             tree = KDTree.new(dimensions, leafsize)
         
@@ -31,7 +42,7 @@ def driver()
                 puts "Please create a tree first"
 
             else 
-                puts "Please Insert list of coordinates seperted by comma, must be of size #{tree.dimensions}"
+                puts "Please Insert list of coordinates seperted by comma, must be of size #{tree.dimensions} ex 6,4,2,..."
                 coord = gets.chomp.split(",").map(&:to_i)
 
                 puts "Assign a code to this entry, must be a string"
@@ -48,6 +59,14 @@ def driver()
             else 
                 puts "How many entries do you want to insert"
                 size = gets.chomp.to_i
+
+                while size <= 0
+                    puts "Invaild mass insertion size must be postive"
+                    size = gets.chomp.to_i
+
+                end
+
+
     
                 for i in 1..size
                     coord = Array.new(tree.dimensions) { rand(1000) } 
@@ -63,7 +82,7 @@ def driver()
             if tree == nil
                 puts "Create a tree first"
             else 
-                puts "Please Insert list of coordinates seperted by comma, must be of size #{tree.dimensions}"
+                puts "Please Insert list of coordinates seperted by comma, must be of size #{tree.dimensions} ex 6,4,2,..."
                 coord = gets.chomp.split(",").map(&:to_i)
 
                 puts "Assign a code to this entry, must be a string"
@@ -73,24 +92,42 @@ def driver()
             end
             
         when 5
-            puts "Which coordintates are you looking for (must be size #{tree.dimensions} seperated by commas)"
-            coord = gets.chomp.split(",").map(&:to_i)
 
-            puts "How many neigbor points are you looking for"
-            neigbor = gets.chomp.to_i
+            if tree == nil
+                puts "Create a tree first"
+            else
+                puts "Which coordintates are you looking for (must be size #{tree.dimensions} seperated by commas) ex 6,4,2,..."
+                coord = gets.chomp.split(",").map(&:to_i)
+    
+                puts "How many neigbor points are you looking for"
+                neigbor = gets.chomp.to_i
+                
+                while neigbor <= 0 || neigbor > 100
+                    puts "Invalid number of neigbor keep between 1 and 100"
+                    neigbor = gets.chomp.to_i
 
-            result, checks = tree.knn(neigbor, coord)
+                end
 
-            puts "Checked #{checks} leaves"
-            
-            result.each do |hash|
-                hash.each_pair do |key, value|
-                  puts "#{key} => Squared distance: #{value}"
+
+                result, checks = tree.knn(neigbor, coord)
+    
+                puts "Checked #{checks} leaves"
+                
+                result.each do |hash|
+                    hash.each_pair do |key, value|
+                      puts "#{key} => Squared distance: #{value}"
+                    end
                 end
             end
 
         when 6
-            puts tree.toString()
+            if tree == nil
+                puts "Create a tree first"
+            else
+                puts tree.toString()
+
+            end
+
     
         else 
             puts "Invalid Option"
---
+++ readme.txt
@@ -0,0 +1,2 @@
+This mini project implmentated a entented KD tree in ruby based on the definition provided in the pdfs.
+Brief TLDR of a KD tree is a data structure that stores multiple dimension. A standard binary tree is stuctured based on a speific value it is 1 dimentional, an extended kd tree may repersent 2 points on a graph, 3 points on a 3d graph, etc. K dimentions total.
